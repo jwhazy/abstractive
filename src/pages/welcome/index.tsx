d8b4a041c2465007918353c48b56e097baee2694
jacksta <63988891+jwhazy@users.noreply.github.com>
2022-09-07 16:22:59 +1200

    chore: eslint fix

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,14 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 
-const Welcome = () => {
+function Welcome() {
   const navigate = useNavigate();
 
   const start = () => navigate("/welcome/client");
 
   return (
     <div className="flex justify-center h-[90vh]">
-      <div className="m-auto text-center space-y-4">
+      <div className="m-auto  text-center space-y-4">
         <div className="animate__animated animate__fadeInDown">
           <h1>Welcome to Abstractive</h1>
           <p>We're glad you're here. Let's get started.</p>
@@ -21,6 +21,6 @@ const Welcome = () => {
       </div>
     </div>
   );
-};
+}
 
 export default Welcome;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [eslint(), react()],
 
   // Vite optons tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
